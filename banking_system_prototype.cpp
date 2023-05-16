ed861a9c15c62a29fbddb7eee36f02a3852720c5
CarlosAlmontes <CarlosAlmontes@csu.fullerton.edu>
2023-05-16 14:48:37 -0700

    Q11 file

---
+++ banking_system_prototype.cpp
@@ -0,0 +1,22 @@
+// Creator: Carlos Almontes
+// Project: Banking system prototype
+// Description: Create prototpe code for specified requirments
+
+
+void login_page(std:string image_link, std::string username, std:string password);
+
+class User{
+public:
+	std::string username;
+	std::string password:
+	std::string history;
+	int balance;
+}
+
+int deposit(std::string username, std:string password, int amount){
+	return int balance;
+}
+
+int withdraw(std::string username, std:string password, int amount){
+	return int balance;
+}
